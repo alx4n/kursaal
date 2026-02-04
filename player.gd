e58cdf807952b3ad4cc757bbcee5247a5f9df1a6
Kevinj-k <masterchief1896@gmail.com>
2026-02-04 14:23:02 -0500

    character movement

---
+++ player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+@export var speed = 100
+
+
+func getInput():
+	var input_direction = Input.get_vector("left","right","up","down")
+	velocity = input_direction * speed
+	
+func _physics_process(delta: float) -> void:
+	getInput()
+	move_and_slide()
