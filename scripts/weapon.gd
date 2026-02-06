185abad34581b9c10064332e3744c2ae4419e35c
Alex Bratek <nbratek99@gmail.com>
2026-02-06 11:03:45 -0500

    Add mouse tracking to weapon

--- scripts/weapon.gd
+++ scripts/weapon.gd
@@ -1,6 +1,7 @@
 extends Sprite2D
 
 const MOUSE_SENSITIVITY = .01
+var mouse_delta = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,12 +11,12 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		self.look_at(get_global_mouse_position())
+		mouse_delta = event.relative
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if mouse_delta > Vector2(0.1, 0.1):
+		self.frame = 1
+		await get_tree().create_timer(0.5).timeout
+		self.frame = 0
 	pass
-
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	pass # Replace with function body.
