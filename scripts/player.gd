125b184fbaf5fa21ba11ef6d44629b7b616c82dd
Kevinj-k <masterchief1896@gmail.com>
2026-02-13 10:06:29 -0500

    removing old test shoot stuff

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,21 +1,15 @@
 extends CharacterBody2D
 @export var speed = 100
-@export var Bullet : Resource
+
 
 
 func getInput():
 	var input_direction = Input.get_vector("left","right","up","down")
 	velocity = input_direction * speed
-	if Input.is_action_just_pressed("shoot"):
-		shoot()
+
 	
 	
 func _physics_process(delta: float) -> void:
 	getInput()
 	move_and_slide()
 	
-	
-func shoot():
-	var b = Bullet.instantiate()
-	owner.add_child(b)
-	b.transform = $Muzzle.global_transform
