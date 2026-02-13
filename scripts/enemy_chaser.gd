f4392889bbb69d90e67c73ae60fc72aeae102572
Cristian Catalano <168125300+Criscatto@users.noreply.github.com>
2026-02-13 11:02:57 -0500

    Enemy attack player detection

--- scripts/enemy_chaser.gd
+++ scripts/enemy_chaser.gd
@@ -1,14 +1,32 @@
 extends CharacterBody2D
 
-var speed = 150
+var speed = 120
 @export var damage_amount: int
 @onready var health_component: HealthComponent = %HealthComponent
 @onready var damage_component: DamageComponent = %DamageComponent
+var collider
+enum States {IDLE, CHASING}
+var state = States.CHASING
 
 func _physics_process(_delta: float) -> void:
-	var player_dir = get_tree().get_first_node_in_group("player").position - self.position
-	self.velocity = player_dir.normalized() * speed
+	if state == States.CHASING:
+		if get_tree().get_first_node_in_group("player") != null:
+			var player_dir = get_tree().get_first_node_in_group("player").position - self.position
+			$AttackDetector.position = player_dir.normalized()
+			self.velocity = player_dir.normalized() * speed
+	else:
+		self.velocity = Vector2(0,0)
+		$AttackDetector.position = self.position
 	move_and_slide()
 	
 func _process(_delta: float) -> void:
 	pass
+
+func _on_attack_detector_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("YEOWCH!")
+		state = States.IDLE
+		$AttackCooldown.start()
+
+func _on_attack_cooldown_timeout() -> void:
+	state = States.CHASING
--- scripts/enemy_sentry.gd
+++ scripts/enemy_sentry.gd
@@ -12,8 +12,9 @@ func _ready() -> void:
 	health_component.set_max_health(100)
 
 func _on_projectile_timer_timeout() -> void:
-	var player_dir = (get_tree().get_first_node_in_group("player").position - self.position).normalized()
-	var new_projectile = projectile_scene.instantiate()
-	get_tree().get_root().add_child(new_projectile)
-	new_projectile.global_position = self.global_position
-	new_projectile.linear_velocity = Vector2(player_dir.x * projectile_speed, player_dir.y * projectile_speed)
+	if get_tree().get_first_node_in_group("player") != null:
+		var player_dir = (get_tree().get_first_node_in_group("player").position - self.position).normalized()
+		var new_projectile = projectile_scene.instantiate()
+		get_tree().get_root().add_child(new_projectile)
+		new_projectile.global_position = self.global_position
+		new_projectile.linear_velocity = Vector2(player_dir.x * projectile_speed, player_dir.y * projectile_speed)
