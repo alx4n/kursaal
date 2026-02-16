9f8c99fe7b1484b3b7e2fd38f29ec26309a5e14b
Cristian Catalano <168125300+Criscatto@users.noreply.github.com>
2026-02-16 11:47:48 -0500

    Enemy dealing damage to player

--- scripts/projectile_sentry.gd
+++ scripts/projectile_sentry.gd
@@ -1,6 +1,8 @@
 extends RigidBody2D
 
+@export var damage_amount = 10
+
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
-		print("OOF!")
+		body.health_component.take_damage(damage_amount)
 		self.queue_free()
