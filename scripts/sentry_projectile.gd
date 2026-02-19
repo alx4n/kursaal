4514708a1758b8fbd2e14ce78e69d77cfe729acb
Alex Bratek <nbratek99@gmail.com>
2026-02-18 22:22:32 -0500

    Rename projectile_sentry to sentry_projectile

--- scripts/sentry_projectile.gd
+++ scripts/sentry_projectile.gd
@@ -1,8 +1,10 @@
 extends RigidBody2D
 
+@onready var damage_component : DamageComponent = %DamageComponent
+
 @export var damage_amount := 20
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
-		print("OOF!")
+		damage_component.deal_damage(damage_amount, body)
 		self.queue_free()
