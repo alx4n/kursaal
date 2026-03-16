6635313855fd3f8abb4bdac6925276b885fb8d36
Kevinj-k <masterchief1896@gmail.com>
2026-03-16 11:23:12 -0400

    working blackjack tfable

--- scenes/blackjack.gd
+++ scenes/blackjack.gd
@@ -1,8 +1,20 @@
 extends Node2D
+var intlist = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
+@onready var playerNumber = $Area2D/playerNumber
+@onready var dealerNumber = $Area2D/dealerNumber
 
 
-@onready var pNumber = $playerNumber
 
-
-func _ready() -> void:
-	pNumber.text = str(randi_range(1,10))
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	var rng = RandomNumberGenerator.new()
+	
+	rng.randomize()
+	
+	var player = rng.randi_range(0,intlist.size()-1)
+	var dealer = rng.randi_range(0,intlist.size()-1)
+	var randInt = intlist[player]
+	var randInt2 = intlist[dealer]
+	
+	playerNumber.text = str(randInt)
+	dealerNumber.text = str(randInt2)
+	
