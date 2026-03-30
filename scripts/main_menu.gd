b8519cd29739211b846d3689f5aa0e1c76b6023f
Cristian Catalano <168125300+Criscatto@users.noreply.github.com>
2026-03-30 11:50:31 -0400

    Menu progress

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,10 +1,25 @@
 extends Node2D
 
+var options = false
+
+func _ready() -> void:
+	$OptionsScreen.hide()
+	options = false
+
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/test.tscn")
+	if !options:
+		get_tree().change_scene_to_file("res://scenes/test.tscn")
 
 func _on_options_button_pressed() -> void:
-	pass # Replace with function body.
+	if !options:
+		options = true
+		$OptionsScreen.show()
 
 func _on_quit_button_pressed() -> void:
-	get_tree().quit()
+	if !options:
+		get_tree().quit()
+
+func _on_back_button_pressed() -> void:
+	if options:
+		$OptionsScreen.hide()
+		options = false
